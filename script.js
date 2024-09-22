1e7b87aab4ec0f420a22130c4bdc48e3984ac18a
Monu Kenvat <monukevat3@gmail.com>
2024-09-22 20:11:07 +0530

    almost completed

--- script.js
+++ script.js
@@ -63,5 +63,28 @@ function bannermove(){
     });
 }
 
+
+function slideFAQ(){
+    const faqs = document.querySelectorAll(".faq")
+
+faqs.forEach((faq)=>
+{
+    faq.addEventListener("click",()=>
+    {
+        if(faq.classList.contains("active")){
+            faq.classList.remove("active")
+        }else{
+            faq.classList.add("active")
+            faqs.forEach((otherfaq)=>{
+                if(otherfaq != faq){
+                    otherfaq.classList.remove("active")
+                }
+            })
+        }
+        })
+})
+
+}
 dropdown();
 bannermove();
+slideFAQ();
